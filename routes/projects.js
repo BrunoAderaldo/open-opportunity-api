6d2d89d4c8ccb46fdf4de3ac045f90c5e39e87a9
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-04-02 17:58:20 -0300

    Adding projects routes

--- routes/projects.js
+++ routes/projects.js
@@ -5,6 +5,16 @@ const project_controller = require('../controllers/projectController');
 /* GET projects listing. */
 router.get('/', project_controller.project_list);
 
-router.post('/', project_controller.project_create_post);
+/* POST projects. */
+router.post('/', project_controller.project_create);
+
+/* GET project detail. */
+router.get('/:projectId', project_controller.project_detail);
+
+/* PATCH project update. */
+router.patch('/:projectId', project_controller.project_update);
+
+/* DELETE project. */
+router.delete('/:projectId', project_controller.project_delete);
 
 module.exports = router;
