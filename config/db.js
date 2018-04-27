8eab4255faba04d9cb0e9d00ab882ac86d0311a9
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-04-27 18:08:47 -0300

    Add enviroment variables

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 let db;
+const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}${process.env.DB_NAME}`;
 
 const dbName = process.env.DB_NAME;
 
 exports.connectDB = callback => {
-  MongoClient.connect(process.env.DB_HOST, (err, client) => {
+  MongoClient.connect(url, (err, client) => {
     assert.equal(null, err);
     console.log(`Connected successfully to ${dbName}`);
 
