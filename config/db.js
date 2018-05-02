cc157ee3242a7768fca16b4ee643e82192d54cd4
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-05-02 00:41:39 -0300

    Verify NODE_ENV

--- config/db.js
+++ config/db.js
@@ -2,12 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 let db;
-const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}${process.env.DB_NAME}`;
+let URL;
+
+if (process.env.NODE_ENV !== 'development') {
+  URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}${process.env.DB_NAME}`;
+} else {
+  URL = process.env.DB_LOCAL;
+}
 
 const dbName = process.env.DB_NAME;
 
 exports.connectDB = callback => {
-  MongoClient.connect(url, (err, client) => {
+  MongoClient.connect(URL, (err, client) => {
     assert.equal(null, err);
     console.log(`Connected successfully to ${dbName}`);
 
