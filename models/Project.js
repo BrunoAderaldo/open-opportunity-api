b23b8e4afbabf4cf5ec05c2ce0fcbb88185a8659
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-05-31 12:32:03 -0300

    Add field userID reference to project owner

--- models/Project.js
+++ models/Project.js
@@ -1,12 +1,13 @@
 const ObjectId = require('mongodb').ObjectID;
 
 class Project {
-  constructor({ category, name, description }) {
+  constructor({ category, name, description, userID }) {
     this._id         = new ObjectId();
     this.createdAt   = new Date();
     this.category    = category;
     this.name        = name;
     this.description = description;
+    this.userID      = userID;
   }
 }
 
