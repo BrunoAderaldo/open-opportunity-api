83e3416e70c3011011189ae0584c47186f0c0245
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2018-09-01 22:07:05 -0300

    Reorder params

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ const ObjectId = require('mongodb').ObjectID;
 class User {
   constructor({ email, password }) {
     this._id       = new ObjectId();
-    this.createdAt = new Date();
     this.email     = email;
     this.password  = password;
+    this.createdAt = new Date();
   }
 }
 
